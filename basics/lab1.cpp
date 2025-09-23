a3ee86696fd8327dba12a953c6a474df4d7f479c
Max <whttmx@gmail.com>
2025-09-24 00:35:39 +0200

    ADD: calculating the area
    ADD:  Calculating the area of a triangle using Heron's formula
    RENAME: from main to lab1

---
+++ basics/lab1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double a,b,c;
+    double half_per;
+    double area;
+    
+    std::cout << "Введите стороны треугольника:" << std::endl;
+    std::cin >> a >> b >> c;
+
+    a = abs(a);
+    b = abs(b);
+    c = abs(c);
+
+    half_per = (a+b+c)/2;
+    area = sqrt(half_per*(half_per - a)*(half_per - b)*(half_per - c));
+
+    std::cout << "Площадь треугольника:" << area << std::endl;
+    std::cout << "double занимает " << sizeof(double) << " бит(а)" << std::endl;
+    return 0;
+
+}
